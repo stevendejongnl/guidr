cfefb25966bdf5b8687ef2e02c8d806501245267
Steven de jong 🍟 <steven@steven-dejong.nl>
2026-01-03 17:39:10 +0100

    fix(Sentry): Disable replay and feedback integrations

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -16,9 +16,12 @@ if (process.env['JEST_WORKER_ID'] === undefined) {
     enableLogs: true,
 
     // Configure Session Replay
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1,
-    integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+    // replaysSessionSampleRate: 0.1,
+    // replaysOnErrorSampleRate: 1,
+    integrations: [
+      // Sentry.mobileReplayIntegration(),
+      // Sentry.feedbackIntegration()
+    ],
 
     // uncomment the line below to enable Spotlight (https://spotlightjs.com)
     // spotlight: __DEV__,
