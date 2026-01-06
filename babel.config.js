742a814c50f9127c32074de11cc0f7b64890350f
Steven de jong 🍟 <steven@steven-dejong.nl>
2026-01-06 10:02:55 +0100

    fix: Add babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@domain': './src/domain',
+          '@infrastructure': './src/infrastructure',
+          '@presentation': './src/presentation',
+          '@common': './src/common',
+        },
+      },
+    ],
+  ],
 }
